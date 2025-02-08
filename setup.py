16fb7f836fb1b149e5bb3472bb0f44a6b1dbf152
Omid Saghatchian <omidsaghatchian@gmail.com>
2025-02-08 22:01:48 +0330

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, Extension
 import os
 
-# Define the C extension with the package name
+# Define the C extension
 module = Extension(
-    "scandirx._scandirx",  # Include package name
+    "scandirx._scandirx",
     sources=["scandirx/scandirx.c"],
-    extra_compile_args=["-O2"],  # Optimization flags (optional)
+    extra_compile_args=["-O2"],
 )
 
 # Read the long description from README.md
@@ -14,6 +14,27 @@ with open(os.path.join(current_dir, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
+    name="scandirx",
+    version="0.1.1",
+    author=["Pedram Parsa", "Omid Saghatchian"],
+    author_email=["titangmz@gmail.com", "omidsaghatchian@gmail.com"],
+    description="A fast directory walker using a C extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/titangmz/scandirx",
+    packages=["scandirx"],
     ext_modules=[module],
-    setup_cfg=True,  # Ensure it reads the setup.cfg
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=[
+        "pytest",
+        "setuptools", 
+        "wheel"
+    ],
+
 )
